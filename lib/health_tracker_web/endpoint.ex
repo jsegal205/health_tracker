8b24c9109fe5db06eaf753808a02346bbcf89741
Jim Segal <hello@jimsegal.com>
2021-04-25 17:43:06 -0500

    mix pow.install and setup

--- lib/health_tracker_web/endpoint.ex
+++ lib/health_tracker_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule HealthTrackerWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :health_tracker
   plug HealthTrackerWeb.Router
 end
--- lib/health_tracker_web/router.ex
+++ lib/health_tracker_web/router.ex
@@ -1,5 +1,6 @@
 defmodule HealthTrackerWeb.Router do
   use HealthTrackerWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -11,6 +12,12 @@ defmodule HealthTrackerWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
   end
 
   scope "/", HealthTrackerWeb do
